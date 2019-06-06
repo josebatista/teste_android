bba96ea46060b2efacd8939d152b03613cd8b6be
Jose Pereira <josebatistapereira@gmail.com>
2019-06-05 21:31:18 -0300

    Finalizado refatoracao de 'articles' mantendo o mesmo padrao que o 'source'.

--- app/src/main/java/dev/dextra/newsapp/feature/news/NewsActivity.kt
+++ app/src/main/java/dev/dextra/newsapp/feature/news/NewsActivity.kt
@@ -4,25 +4,21 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.textfield.TextInputLayout
 import dev.dextra.newsapp.R
 import dev.dextra.newsapp.api.model.Article
 import dev.dextra.newsapp.api.model.Source
 import dev.dextra.newsapp.base.BaseListActivity
 import dev.dextra.newsapp.feature.news.adapter.ArticleListAdapter
 import kotlinx.android.synthetic.main.activity_news.*
-import kotlinx.android.synthetic.main.activity_sources.*
 import org.koin.android.ext.android.inject
 
 
 const val NEWS_ACTIVITY_SOURCE = "NEWS_ACTIVITY_SOURCE"
 
-class NewsActivity : BaseListActivity() {
+class NewsActivity : BaseListActivity(), ArticleListAdapter.ArticleListAdapterItemClick {
 
     override val emptyStateTitle: Int = R.string.empty_state_title_news
     override val emptyStateSubTitle: Int = R.string.empty_state_subtitle_news
@@ -33,11 +29,13 @@ class NewsActivity : BaseListActivity() {
 
     private val newsViewModel: NewsViewModel by inject()
 
-    private var viewAdapter: ArticleListAdapter = ArticleListAdapter(this, this, emptyList())
+    private var viewAdapter: ArticleListAdapter = ArticleListAdapter(this)
     private var viewManager: RecyclerView.LayoutManager = GridLayoutManager(this, 1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_news)
+
+        setupList()
 
         (intent?.extras?.getSerializable(NEWS_ACTIVITY_SOURCE) as Source).let { source ->
             title = source.name
@@ -50,8 +48,13 @@ class NewsActivity : BaseListActivity() {
     }
 
     private fun loadNews(source: Source) {
-        newsViewModel.articles.observe(this, Observer {
-
+        newsViewModel.articles.observe(this, Observer { articles ->
+            viewAdapter.apply {
+                articles?.let {
+                    clear()
+                    add(it)
+                }
+            }
         })
 
         newsViewModel.networkState.observe(this, networkStateObserver)
@@ -60,59 +63,30 @@ class NewsActivity : BaseListActivity() {
         newsViewModel.loadNews()
     }
 
-    fun onClick(article: Article) {
+    override fun onClick(article: Article) {
         val i = Intent(Intent.ACTION_VIEW)
         i.data = Uri.parse(article.url)
         startActivity(i)
     }
 
-//    private var loading: Dialog? = null
-//
-//    fun showLoading() {
-//        if (loading == null) {
-//            loading = Dialog(this)
-//            loading?.apply {
-//                requestWindowFeature(Window.FEATURE_NO_TITLE)
-//                window.setBackgroundDrawableResource(android.R.color.transparent)
-//                setContentView(R.layout.dialog_loading)
-//            }
-//        }
-//        loading?.show()
-//    }
-//
-//    fun hideLoading() {
-//        loading?.dismiss()
-//    }
-
-    fun showData(articles: List<Article>) {
-        val viewAdapter = ArticleListAdapter(this@NewsActivity, this@NewsActivity, articles)
-        news_list.adapter = viewAdapter
+    private fun setupList() {
+        news_list.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 
     override fun setupPortrait() {
         setListColumns(1)
-        sources_filters.orientation = LinearLayout.VERTICAL
-        configureFilterLayoutParams(country_select_layout, ViewGroup.LayoutParams.MATCH_PARENT, 0f)
-        configureFilterLayoutParams(category_select_layout, ViewGroup.LayoutParams.MATCH_PARENT, 0f)
     }
 
     override fun setupLandscape() {
         setListColumns(2)
-        sources_filters.orientation = LinearLayout.HORIZONTAL
-        configureFilterLayoutParams(country_select_layout, 0, 1f)
-        configureFilterLayoutParams(category_select_layout, 0, 1f)
-    }
-
-    private fun configureFilterLayoutParams(textInput: TextInputLayout, width: Int, weight: Float) {
-        val layoutParams = textInput.layoutParams
-        if (layoutParams is LinearLayout.LayoutParams) {
-            layoutParams.width = width
-            layoutParams.weight = weight
-        }
     }
 
     private fun setListColumns(columns: Int) {
-        val layoutManager = sources_list.layoutManager
+        val layoutManager = news_list.layoutManager
         if (layoutManager is GridLayoutManager) {
             layoutManager.spanCount = columns
             viewAdapter.notifyDataSetChanged()
